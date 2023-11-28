b0e051b51b10e0b711896517db9d377bbd823623
blvck-root <mtndlovu81@gmail.com>
2023-11-28 18:28:11 +0200

    correction of tasks 0-12

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
-	for j in range(i, 10):
-		if i == 8 and j == 9:
-			print(f"{(i*10 + j):02d}")
-		elif (i != j):
-			print(f"{(i*10 + j):02d},", end=" ")
+    for j in range(i, 10):
+        if i == 8 and j == 9:
+            print("{:02d}".format(i*10 + j))
+        elif (i != j):
+            print("{:02d},".format(i*10 + j), end=" ")
