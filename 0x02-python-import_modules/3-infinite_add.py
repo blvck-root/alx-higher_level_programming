f5aad8e62f38f141fbc62929664891a6c0196bfd
blvck-root <mtndlovu81@gmail.com>
2023-11-30 15:35:14 +0200

    change variable name

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    sum_ = 0
+    total = 0
     for i in range(1, len(argv)):
-        sum_ += int(argv[i])
-    print(sum_);
+        total += int(argv[i])
+    print(total);
