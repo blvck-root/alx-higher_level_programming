22c726b5160d489ef7c23a0acb90c774a7f7b156
blvck-root <mtndlovu81@gmail.com>
2023-12-02 10:48:08 +0200

    use format specifier to print integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[]):
     for row in matrix:
         for i in range(len(row) - 1):
             print("{:d}".format(row[i]), end=" ")
-        print("{}".format(row[-1]))
+	print("{:d}".format(row[-1]))
