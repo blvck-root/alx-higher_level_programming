2047d2425bce731fa7f1615a697cfe750d559136
blvck-root <mtndlovu81@gmail.com>
2023-12-02 11:35:42 +0200

    update 7-add_tuple and 3-print_reversed_list_integer

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-def fill_with_zero(tuple_):
-    """Return tuple filled with zeros
-    if tuple_ has fewer than 2 elements"""
-    if len(tuple_) == 0:
-        return (0, 0)
-    elif len(tuple_) == 1:
-        return (tuple_[0], 0)
-    else:
-        return tuple_
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = fill_with_zero(tuple_a)
-    b = fill_with_zero(tuple_b)
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
     return tuple([a[i] + b[i] for i in range(2)])
