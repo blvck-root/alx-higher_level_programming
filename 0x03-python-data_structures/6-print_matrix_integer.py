de79d4d36734a336fba5b8f1ab04fe1bc7bd03ab
blvck-root <mtndlovu81@gmail.com>
2023-12-03 13:02:25 +0200

    update default matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[]):
-    if matrix:
+def print_matrix_integer(matrix=[[]]):
+    if matrix[0]:
         for row in matrix:
             for i in range(len(row) - 1):
                 print("{:d}".format(row[i]), end=" ")
