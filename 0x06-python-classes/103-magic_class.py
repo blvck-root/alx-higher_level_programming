a19889622bc8e478219b6036612ec40a46900074
blvck-root <mtndlovu81@gmail.com>
2023-12-20 15:19:23 +0200

    add docs for MagicClass

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,14 @@
+#!/usr/bin/python3
+"""Decode Python ByteCode"""
+
+
+import math
+
+
 class MagicClass:
+    """Define MagicClass"""
     def __init__(self, radius):
+        """Initialize MagicClass"""
         self.radius = 0
 
         if type(radius) is not int or type(radius) is not float:
