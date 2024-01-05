48ed8442d923c9fa39f21fd37f1a2af3367b3aaa
blvck-root <mtndlovu81@gmail.com>
2024-01-05 14:56:28 +0200

    fix errors

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,10 +3,15 @@
 101-locked_class module contains the LockedClass.
 """
 
+
 class LockedClass:
     """Blueprint for the LockedClass class."""
+    __slots__ = ("first_name",)
+
     def __setattr__(self, name, value):
         """This function is called when a new instance attribute is set."""
         if name != "first_name":
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
-        self.__dict__[name] = value
+            raise AttributeError(
+                f"'LockedClass' object has no attribute '{name}'"
+            )
+        super().__setattr__(name, value)
