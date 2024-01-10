f922c8c2fa5115d71a84cf8187f7db483294f8f3
blvck-root <mtndlovu81@gmail.com>
2024-01-10 12:30:23 +0200

    read, write, append

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,7 @@
 """
 
 def read_file(filename=""):
+    """Read and print a file"""
     with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             print(line, end="")
