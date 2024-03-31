02b22324ea593e0723ebf4df9c9e055d01deda61
blvck-root <mtndlovu81@gmail.com>
2024-03-31 10:02:50 +0200

    fixed PEP8 violations

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 class City(Base):
     """ City class"""
     __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -9,6 +9,8 @@ from relationship_city import Base, City
 class State(Base):
     """ State class"""
     __tablename__ = "states"
-    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
