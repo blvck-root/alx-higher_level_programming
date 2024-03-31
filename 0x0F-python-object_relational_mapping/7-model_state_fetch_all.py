ca041dad161ad9ab652f5ed9f8e406a2a86044ca
blvck-root <mtndlovu81@gmail.com>
2024-03-31 09:41:46 +0200

    add relationship modules

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,13 +3,13 @@
 Task 7
 Write a script that lists all State objects from the database hbtn_0e_6_usa
 """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 
 if __name__ == "__main__":
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
-
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
         pool_pre_ping=True)
